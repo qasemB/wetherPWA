67d1e3680d905a9fb0d6db17e37a64d89f7c767f
qasemB <mahdicmptr@gmail.com>
2023-06-27 00:26:50 +0330

    part #39

--- src/SwDev.js
+++ src/SwDev.js
@@ -1,14 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const SwDev = () => {
+    const [showInstallTag, setShowInstallTag] = useState(true)
+    let deferredPrompt;
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("/sw.js").then((res) => {
         console.log("registered...");
       });
     }
-  }, []);
-  return null;
+
+
+    window.addEventListener("beforeinstallprompt", (e) => {
+        console.log("beforeinstallprompt");
+      e.preventDefault();
+      deferredPrompt = e;
+      return false;
+    });
+    
+}, []);
+
+    const handleShowInstallPrompt = ()=>{
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            setShowInstallTag(false)
+            deferredPrompt.userChoice.then((choiceRes) => {
+              console.log(choiceRes.outcome);
+              if (choiceRes.outcome === "accepted") {
+                console.log("User accepted the install prompt.");
+              } else if (choiceRes.outcome === "dismissed") {
+                console.log("User dismissed the install prompt");
+              }
+            });
+            deferredPrompt = null;
+          }
+    }
+
+  return (
+    <>
+    {
+        showInstallTag && (
+            <div style={{position:"fixed", width:"100%", height:"50px", background:"blue", color:"white", bottom:"0", textAlign:"center", paddingTop:"15px"}} onClick={handleShowInstallPrompt}>
+                Please install this app...
+            </div>
+        )
+    }
+    </>
+  );
 };
 
 export default SwDev;
