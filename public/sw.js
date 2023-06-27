a74a30b094167a7f07194071a7d0cc3e357577cc
qasemB <mahdicmptr@gmail.com>
2023-06-27 21:24:21 +0330

    part #40

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,35 @@
-self.addEventListener('install', ()=>{
-    console.log('installed...');
-})
+const staticItems = [
+    "/",
+    "/index.html",
+    "/static/js/main.chunk.js",
+    "/static/js/vendors~main.chunk.js",
+    "/static/js/bundle.js",
+    "/assets/css/bootstrap.css",
+    "/assets/css/bootstrap.min.css",
+    "/assets/fontawesome/css/all.css",
+    "/assets/css/style.css",
+    "/manifest.json",
+    "assets/images/weather/usual-mini.jpg"
+]
+const STATIC_CACHE = "cache_v1"
+
+self.addEventListener("install", function(e){
+    e.waitUntil(
+        caches.open(STATIC_CACHE).then(cache=>{
+            cache.addAll(staticItems)
+        })
+    )
+}) 
 
 self.addEventListener('activate', ()=>{
-    console.log("activateed...");
+    // console.log("activateed...");
 })
 
 self.addEventListener('fetch', (e)=>{
-    console.log("fetch:", e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(res=>{
+            return res || fetch(e.request)
+        })
+    )
+    // console.log("fetch:", e.request.url);
 })
