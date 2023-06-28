70f75963009721c7fe9bfa6ed4e65ca3b9db89df
qasemB <mahdicmptr@gmail.com>
2023-06-28 15:56:20 +0330

    part #41

--- public/sw.js
+++ public/sw.js
@@ -9,16 +9,16 @@ const staticItems = [
     "/assets/fontawesome/css/all.css",
     "/assets/css/style.css",
     "/manifest.json",
-    "assets/images/weather/usual-mini.jpg"
+    "/assets/images/weather/usual-mini.jpg"
 ]
 const STATIC_CACHE = "cache_v1"
 
 self.addEventListener("install", function(e){
-    e.waitUntil(
-        caches.open(STATIC_CACHE).then(cache=>{
-            cache.addAll(staticItems)
-        })
-    )
+    // e.waitUntil(
+    //     caches.open(STATIC_CACHE).then(cache=>{
+    //         cache.addAll(staticItems)
+    //     })
+    // )
 }) 
 
 self.addEventListener('activate', ()=>{
@@ -26,10 +26,34 @@ self.addEventListener('activate', ()=>{
 })
 
 self.addEventListener('fetch', (e)=>{
-    e.respondWith(
-        caches.match(e.request).then(res=>{
-            return res || fetch(e.request)
-        })
-    )
+    // e.respondWith(
+    //     caches.match(e.request).then(res=>{
+    //         return res || fetch(e.request)
+    //     })
+    // )
     // console.log("fetch:", e.request.url);
+})
+
+
+//notificatio--------------
+self.addEventListener('push', (event)=>{
+    const notification = event.data.json()
+    const options = {
+        body: notification.body,
+        icon: "/assets/images/codeyadIcon.png",
+        image: "/assets/images/office.jpg",
+        dir: "ltr",
+        vibrate: [100, 50, 200],
+        badge: "/assets/images/codeyadIcon.png",
+        tag: "group1",
+        renotify: true,
+        actions: [
+          {action: "confirm", title: "تایید", icon:"/assets/images/confirm.png"},
+          {action: "cancel", title: "انصراف", icon:"/assets/images/cancel.png"}
+        ],
+        data:{
+            notifUrl: notification.url
+        }
+      }  
+    self.registration.showNotification(notification.title, options)
 })
--- src/SwDev.js
+++ src/SwDev.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const SwDev = () => {
     const [showInstallTag, setShowInstallTag] = useState(true)
+    const [showNotifPermTag, setShowNotifPermTag] = useState(false)
     let deferredPrompt;
   useEffect(() => {
     if ("serviceWorker" in navigator) {
@@ -17,6 +18,11 @@ const SwDev = () => {
       deferredPrompt = e;
       return false;
     });
+
+    // notification-------------
+    if (Notification.permission == "default") {
+      setShowNotifPermTag(true)
+    }
     
 }, []);
 
@@ -35,6 +41,37 @@ const SwDev = () => {
             deferredPrompt = null;
           }
     }
+    
+    const showConfirmNotify = ()=>{
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.ready.then(sw=>{
+          sw.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: "BNbqX8M5NJJfs_IcL_5Gfisx7FkOYHtYniD4QMJq1RB4DeQsOmGo3lO-zzurFEqTUwtrqQHKb62p_TzxPU552yI"
+          }).then(subscription=>{
+            fetch("https://pushnotif.azhadev.ir/api/push-subscribe",{
+              method: "post",
+              body: JSON.stringify(subscription),        
+            }).then(res=>{
+              return res.json()
+            }).then(response=>{
+              console.log(response);
+              alert("این کد رو ذخیره کنید : "+ response.user_code)
+            })
+          })
+        })
+      }
+    }
+
+    const handleShowNotifPermission = ()=>{
+      Notification.requestPermission((res)=>{
+        if (res == "granted") {
+          showConfirmNotify()
+        }else{
+          console.log("Blocked...!");
+        }
+      })
+    }
 
   return (
     <>
@@ -43,7 +80,15 @@ const SwDev = () => {
             <div style={{position:"fixed", width:"100%", height:"50px", background:"blue", color:"white", bottom:"0", textAlign:"center", paddingTop:"15px"}} onClick={handleShowInstallPrompt}>
                 Please install this app...
             </div>
-        )
+        ) 
+    }
+
+    {
+      showNotifPermTag && (
+        <div style={{position:"fixed", width:"100%", height:"50px", background:"gray", color:"white", bottom:"50px", textAlign:"center", paddingTop:"15px"}} onClick={handleShowNotifPermission}>
+                Please click here to receive notifications
+        </div>
+      )
     }
     </>
   );
